7afa17e0690c37fe56385e50168b852fda727ccf
Nils_newbie <sorrentosgeorge@gmail.com>
2025-04-13 18:22:13 +0200

    Update: Fragenkatalog in questions.py angepasst

--- questions.py
+++ questions.py
@@ -6,14 +6,14 @@ Created on Sun Apr 13 12:11:36 2025
 """
 
 questions = [
-    {"question": "Testfrage 1: Funktioniert es?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Testfrage 2: Geht es weiter?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
-    {"question": "Wird die Frage angezeigt?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 1: Huh?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 2: Hah?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 3: Weiter?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 4: Was ist richtig?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 5: Steht hier Ja?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 6: Nein?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 7: Ist das eine Frage?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 8: Bereit für das nächste Minispiel?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 9: Ist das die letzte Frage?", "options": ["ja", "nein"], "correct": "ja"},
+    {"question": "Frage 10: Und jetzt?", "options": ["ja", "nein"], "correct": "ja"},
 ]
