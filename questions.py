b638d8ad486a85e6883bec95399719263d971737
Nils_newbie <sorrentosgeorge@gmail.com>
2025-04-20 15:36:18 +0200

    Update: Minispiel 5 responsive + Bildoptimierungen

--- questions.py
+++ questions.py
@@ -6,16 +6,16 @@ Created on Sun Apr 13 12:11:36 2025
 """
 
 questions = [
-    {"question": "Frage 1: Huh?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 2: Hah?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 3: Weiter?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 4: Was ist richtig?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 5: Steht hier Ja?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 6: Ja oder Nein?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 7: ?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 8: Das ist Frage 8!", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 9: Hast du bisher alles richtig gemacht?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 10: Ein letztes Minispiel bitte noch!", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 11: Traurig?", "options": ["Ja", "Nein"], "correct": "Ja"},
-    {"question": "Frage 12: Ist das die letzte Frage?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 1: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 2: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 3: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 4: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 5: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 6: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 7: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 8: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 9: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 10: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 11: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
+    {"question": "Frage 12: ..?", "options": ["Ja", "Nein"], "correct": "Ja"},
 ]
