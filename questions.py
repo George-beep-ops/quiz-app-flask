f8ddb7e393be7ad2cfac95f337fbb0537cf44d4b
George-beep-ops <sorrentosgeorge@gmail.com>
2025-04-29 14:55:44 +0200

    Meine Änderungen vom neuen PC

--- questions.py
+++ questions.py
@@ -6,15 +6,15 @@ Created on Sun Apr 13 12:11:36 2025
 """
 
 questions = [
-    {"question": "Question 1: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 2: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 3: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 4: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 5: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 6: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 7: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 8: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 9: ..?", "options": ["Yes", "No"], "correct": "Yes"},
-    {"question": "Question 10: ..?", "options": ["Yes", "No"], "correct": "Yes"},
+    {"question": "Question 1: Are Popular video games like FIFA often part of gamified learning?", "options": ["Yes", "No"], "correct": "No"},
+    {"question": "Question 2: Is gamification about adding game-like elements to non-game contexts?", "options": ["Yes", "No"], "correct": "Yes"},
+    {"question": "Question 3: Is Alfred Binet the founder of the social cognitive load theory?", "options": ["Yes", "No"], "correct": "No"},
+    {"question": "Question 4: According to Social Cognitive Theory, do emotions triggered by competition decrease attention?", "options": ["Yes", "No"], "correct": "No"},
+    {"question": "Question 5: Is the Cognitive Load Theory mainly concerned with optimizing learning by managing mental effort?", "options": ["Yes", "No"], "correct": "Yes"},
+    {"question": "Question 6: Does the Self-Determination Theory link autonomy to offering choices in gamified environments?", "options": ["Yes", "No"], "correct": "Yes"},
+    {"question": "Question 7: Does the presentation recommend using gamification mainly as an afterthought once all learning materials are completed?", "options": ["Yes", "No"], "correct": "No"},
+    {"question": "Question 8: Is defining clear learning objectives a tip for successful gamification implementation?", "options": ["Yes", "No"], "correct": "Yes"},
+    {"question": "Question 9: Is it stated that motivation from gamification will automatically remain high without any need for further effort?", "options": ["Yes", "No"], "correct": "No"},
+    {"question": "Question 10: Can peer competition during gamified learning sessions increase emotional engagement?", "options": ["Yes", "No"], "correct": "Yes"},
 
 ]
