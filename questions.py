1bb73367f2539e6cb436ae1ad0841b50635a5472
George-beep-ops <sorrentosgeorge@gmail.com>
2025-05-01 16:08:27 +0200

    Meine Änderungen vom neuen PC

--- questions.py
+++ questions.py
@@ -6,7 +6,7 @@ Created on Sun Apr 13 12:11:36 2025
 """
 
 questions = [
-    {"question": "Question 1: Are Popular video games like FIFA often part of gamified learning?", "options": ["Yes", "No"], "correct": "No"},
+    {"question": "Question 1: Are popular video games like FIFA often part of gamified learning?", "options": ["Yes", "No"], "correct": "No"},
     {"question": "Question 2: Is gamification about adding game-like elements to non-game contexts?", "options": ["Yes", "No"], "correct": "Yes"},
     {"question": "Question 3: Is Alfred Binet the founder of the social cognitive load theory?", "options": ["Yes", "No"], "correct": "No"},
     {"question": "Question 4: According to Social Cognitive Theory, do emotions triggered by competition decrease attention?", "options": ["Yes", "No"], "correct": "No"},
